fbaaf5ad4b578a5b9818b967329be2247020d9de
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-06-02 23:30:29 +0530

    Navbar Updated

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,11 +1,11 @@
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white  text-2xl sm:hidden md:block">
-        Abdelrahman Qassem
+      <h1 className="text-white  text-4xl sm:hidden md:block">
+        Mohd Shariq
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        MS
       </h1>
     </div>
   );
