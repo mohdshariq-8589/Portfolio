d0336a7ef2d37a308b0821a2ca2dc9fe2c6d87c7
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-06-02 23:19:12 +0530

    About section Updated

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -5,13 +5,7 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p>
-        I’m Abdelrahman, a web developer instructor with a passion for teaching
-        and coding. I specialize in React and front-end development, helping
-        students build real-world projects and master modern web technologies. I
-        also run a YouTube channel, CodeNest, where I create tutorials and
-        courses to guide aspiring developers in their journey toward successful
-        careers in tech. Outside of coding, I enjoy continuous learning and
-        sharing knowledge to inspire others to achieve their goals.
+         I’m Mohd Shariq, a full-stack developer with a passion for building modern, user-friendly web applications. I mainly work with the MERN stack — MongoDB, Express.js, React, and Node.js — and love creating clean, responsive UIs using tools like Tailwind CSS and Zustand. On the backend, I enjoy working with databases, APIs, and integrations like Stripe and Cloudinary. I’ve recently completed my MCA from MMMUT, Gorakhpur, and I’m always looking to learn new things and take on exciting projects
       </p>
       <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
         <Link
