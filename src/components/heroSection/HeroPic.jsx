b20e2f1a0476a88fb811eeb8630bd43ea24ab5d1
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-06-05 12:42:19 +0530

    Update the hero section

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
-import { PiHexagonThin } from "react-icons/pi";
+// import { PiHexagonThin } from "react-icons/pi";
 
 const HeroPic = () => {
   return (
@@ -12,14 +12,14 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/HexaPic.png"
-        alt="Abdelrahman Qassem"
-        className="max-h-[450px] w-auto"
+        src="../../public/images/MS.jpg"
+        alt="Mohd Shariq"
+        className="max-h-[450px] w-auto rounded-lg"
       />
 
-      <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
+      {/* <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
-      </div>
+      </div> */}
     </motion.div>
   );
 };
--- src/components/heroSection/HeroText.jsx
+++ src/components/heroSection/HeroText.jsx
@@ -11,7 +11,7 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="lg:text-2xl sm:text-xl  uppercase text-lightGrey "
       >
-        Front-End Web Developer
+        Full Stack Developer
       </motion.h2>
       <motion.h1
         variants={fadeIn("right", 0.4)}
@@ -20,8 +20,8 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="md:text-[2.8rem] lg:text-6xl sm:text-4xl text-orange font-bold uppercase"
       >
-        Abdelrahman <br className="sm:hidden md:block" />
-        Qassem
+        Mohd <br className="sm:hidden md:block" />
+        Shariq
       </motion.h1>
       <motion.p
         variants={fadeIn("up", 0.6)}
@@ -30,8 +30,8 @@ const HeroText = () => {
         viewport={{ once: false, amount: 0 }}
         className="text-lg mt-4"
       >
-        A Passionate Web Developer and Instructor <br /> with 3 years of
-        experience.
+        Full-stack developer with a strong focus on building scalable,
+       responsive web applications.
       </motion.p>
     </div>
   );
